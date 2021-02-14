5b1f4cb7ef0d47e2c283d4f0d6229af5a057df4d
Saviour1001 <harshghodkar@gmail.com>
2021-02-14 20:15:02 +0530

    Push

---
+++ Coding wala love/A.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>  
+using namespace std;
+ 
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> p32;
+typedef pair<ll,ll> p64;
+typedef pair<double,double> pdd;
+typedef vector<ll> v64;
+typedef vector<int> v32;
+typedef vector<vector<int> > vv32;
+typedef vector<vector<ll> > vv64;
+typedef vector<vector<p64> > vvp64;
+typedef vector<p64> vp64;
+typedef vector<p32> vp32;
+ll MOD = 998244353;
+double eps = 1e-12;
+#define forn(i,e) for(ll i = 0; i < e; i++)
+#define forsn(i,s,e) for(ll i = s; i < e; i++)
+#define rforn(i,s) for(ll i = s; i >= 0; i--)
+#define rforsn(i,s,e) for(ll i = s; i >= e; i--)
+#define ln "\n"
+#define dbg(x) cout<<#x<<" = "<<x<<ln
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define INF 2e18
+#define flash ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+ 
+
+vector<bool> segmentedSieve(long long L, long long R) {      //Generates primes between L,R             
+ long long lim = sqrt(R);
+ vector<bool> mark(lim + 1, false);
+vector<long long> primes;
+ for (long long i = 2; i <= lim; ++i) {
+ if (!mark[i]) {
+ primes.emplace_back(i);
+for (long long j = i * i; j <= lim; j += i)
+mark[j] = true;
+}
+}
+vector<bool> isPrime(R - L + 1, true);
+for (long long i : primes)
+for (long long j = max(i * i, (L + i - 1) / i * i); j <= R; j += i)
+isPrime[j - L] = false;
+if (L == 1)
+isPrime[0] = false;
+return isPrime;
+}
+
+int makeStringEmpty(string s) {
+    string finder="geek";
+    int counter=0;
+    bool check=1;
+    int i=0;
+    while(i<s.length() && check)
+    {
+        if(s[i]=='g' && i+3<s.length() && s[i+1]=='e' && s[i+2]=='e' && s[i+3]=='k')
+        {
+            s=s.erase(i,4);
+            counter++;
+            i=0;
+            continue;
+        }
+        i++;
+        
+    }
+    if(s.length()==0)
+    {
+        return counter;
+    }
+    else
+    return -1;
+    
+
+
+    }
+
+
+int main()
+{
+
+    flash;
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        cout<<makeStringEmpty(s)<<ln;
+    }
+    return 0;
+}
