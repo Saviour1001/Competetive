cba284b2da0356bdb403de5dc6c0a7d8418bc79a
Saviour1001 <harshghodkar@gmail.com>
2021-05-26 20:10:19 +0530

    yo

---
+++ Virtual Contests/CF 693/A.cpp
@@ -0,0 +1,103 @@
+#include "bits/stdc++.h"
+using namespace std;
+#define int               long long
+#define pb                push_back
+#define ppb               pop_back
+#define pf                push_front
+#define ppf               pop_front
+#define all(x)            (x).begin(),(x).end()
+#define uniq(v)           (v).erase(unique(all(v)),(v).end())
+#define sz(x)             (int)((x).size())
+#define fr                first
+#define sc                second
+#define pii               pair<int,int>
+#define rep(i,a,b)        for(int i=a;i<b;i++)
+#define mem1(a)           memset(a,-1,sizeof(a))
+#define mem0(a)           memset(a,0,sizeof(a))
+#define ppc               __builtin_popcount
+#define ppcll             __builtin_popcountll
+#define ln "\n"
+
+
+template<typename T1,typename T2>istream& operator>>(istream& in,pair<T1,T2> &a){in>>a.fr>>a.sc;return in;}
+template<typename T1,typename T2>ostream& operator<<(ostream& out,pair<T1,T2> a){out<<a.fr<<" "<<a.sc;return out;}
+template<typename T,typename T1>T amax(T &a,T1 b){if(b>a)a=b;return a;}
+template<typename T,typename T1>T amin(T &a,T1 b){if(b<a)a=b;return a;}
+const long long INF=1e18;
+const int32_t M=1e9+7;
+const int32_t MM=998244353;
+const int N=0;
+
+long long power(long long a, long long b,int MOD=M) {
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % MOD;
+        a = a * a % MOD;
+        b >>= 1;
+    }
+    return res;
+}
+
+bool checker(int n)
+{
+    return !(n&n-1);
+}
+void solve(int test){
+    int w,h,n;
+    cin>>w>>h>>n;
+    int sheets=0;
+    if(w%2==0)
+    {
+        if(checker(w))
+        {
+            while(w)
+            {
+                w/=2;
+                sheets+=1;
+            }
+        }
+        else
+        sheets++;
+    }
+    if(h%2==0)
+    {
+        if(checker(h))
+        {
+            while(h)
+            {
+                h/=2;
+                sheets+=1;
+            }
+        }
+        else
+        sheets++;
+    }
+    n=n-power(2,sheets);
+    if(n<=0)
+    {
+        cout<<"YES\n";
+    }
+    else
+    {
+        cout<<"NO\n";
+    }
+}
+
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    #ifdef SIEVE
+    sieve();
+    #endif
+    #ifdef NCR
+    init();
+    #endif
+    int t=1;
+    cin>>t;
+    rep(i,1,t+1) solve(i);
+    return 0;
+}
