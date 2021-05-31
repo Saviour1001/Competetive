9222585868436cd3473a0981e05f2a3e86bca10d
Saviour1001 <harshghodkar@gmail.com>
2021-05-31 22:58:05 +0530

    lunchtime

---
+++ May Lunchtime/C.cpp
@@ -0,0 +1,76 @@
+#include "bits/stdc++.h"
+using namespace std;
+#define int               long long
+#define pb                push_back
+#define ppb               pop_back
+#define pf                push_front
+#define ppf               pop_front
+#define all(x)            (x).begin(),(x).end()
+#define uniq(v)           (v).erase(unique(all(v)),(v).end())
+#define sz(x)             (int)((x).size())
+#define fr                first
+#define sc                second
+#define pii               pair<int,int>
+#define rep(i,a,b)        for(int i=a;i<b;i++)
+#define mem1(a)           memset(a,-1,sizeof(a))
+#define mem0(a)           memset(a,0,sizeof(a))
+#define ppc               __builtin_popcount
+#define ppcll             __builtin_popcountll
+#define ln "\n"
+
+
+template<typename T1,typename T2>istream& operator>>(istream& in,pair<T1,T2> &a){in>>a.fr>>a.sc;return in;}
+template<typename T1,typename T2>ostream& operator<<(ostream& out,pair<T1,T2> a){out<<a.fr<<" "<<a.sc;return out;}
+template<typename T,typename T1>T amax(T &a,T1 b){if(b>a)a=b;return a;}
+template<typename T,typename T1>T amin(T &a,T1 b){if(b<a)a=b;return a;}
+const long long INF=1e18;
+const int32_t M=1e9+7;
+const int32_t MM=998244353;
+const int N=0;
+
+vector<bool> segmentedSieve(long long L, long long R) {      //Generates primes between L,R             
+ long long lim = sqrt(R);
+ vector<bool> mark(lim + 1, false);
+vector<long long> primes;
+ for (long long i = 2; i <= lim; ++i) {
+ if (!mark[i]) {
+ primes.emplace_back(i);
+for (long long j = i * i; j <= lim; j += i)
+mark[j] = true;
+}
+}
+vector<bool> isPrime(R - L + 1, true);
+for (long long i : primes)
+for (long long j = max(i * i, (L + i - 1) / i * i); j <= R; j += i)
+isPrime[j - L] = false;
+if (L == 1)
+isPrime[0] = false;
+return isPrime;
+}
+
+void solve(int test){
+    int n;
+    cin>>n;
+    int count=1;
+    
+    cout<<count<<ln;
+
+}
+
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    #ifdef SIEVE
+    sieve();
+    #endif
+    #ifdef NCR
+    init();
+    #endif
+    int t=1;
+    cin>>t;
+    rep(i,1,t+1) solve(i);
+    return 0;
+}
